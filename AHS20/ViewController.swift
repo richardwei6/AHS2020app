af8dab5923520b91323daef05aaaf9683c76ed0b
richardwei6 <48197502+richardwei6@users.noreply.github.com>
2020-03-14 11:58:14 -0700

    Inital Setup

---
+++ AHS20/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AHS20
+//
+//  Created by Richard Wei on 3/14/20.
+//  Copyright © 2020 AHS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
