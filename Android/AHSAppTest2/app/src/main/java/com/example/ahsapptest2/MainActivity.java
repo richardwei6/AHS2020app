79ff8c91852ffcb065309312122c2beac56d3630
richy6 <richardwei6@gmail.com>
2020-03-17 20:18:10 -0700

    Image Scrollview implemented

--- Android/AHSAppTest2/app/src/main/java/com/example/ahsapptest2/MainActivity.java
+++ Android/AHSAppTest2/app/src/main/java/com/example/ahsapptest2/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.ahsapptest2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
@@ -10,5 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
+
 }
--- Android/AHSAppTest2/app/src/main/java/com/example/ahsapptest2/News_MainPage.java
+++ Android/AHSAppTest2/app/src/main/java/com/example/ahsapptest2/News_MainPage.java
@@ -1,9 +1,9 @@
 package com.example.ahsapptest2;
 
-
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +12,7 @@ import android.view.ViewGroup;
 
 
 public class News_MainPage extends Fragment {
+
 
     public News_MainPage() {
         // Required empty public constructor
