d56b99ae367f8c8c84e35eddce1e4a8d6b70db72
richardwei6 <48197502+richardwei6@users.noreply.github.com>
2020-04-08 16:56:20 -0700

    Temporary Update

--- IOS/AHS20/bulletinPage.swift
+++ IOS/AHS20/bulletinPage.swift
@@ -10,9 +10,35 @@ import Foundation
 import UIKit
 
 class bulletinClass: UIViewController, UIScrollViewDelegate, UITabBarControllerDelegate {
-
+    @IBOutlet weak var seniorButton: UIButton!
+    @IBOutlet weak var collegeButton: UIButton!
+    @IBOutlet weak var athleticsButton: UIButton!
+    @IBOutlet weak var eventsButton: UIButton!
+    @IBOutlet weak var referenceButton: UIButton!
+    @IBOutlet weak var otherButton: UIButton!
+    
+    var buttonTextEdgeInset = UIEdgeInsets(top: 0, left: 45, bottom: 0, right: 0);
+    
     override func viewDidLoad() {
         super.viewDidLoad();
+        
+        // set up correct spacing between text on button and image
+        seniorButton.contentEdgeInsets = buttonTextEdgeInset;
+        seniorButton.contentHorizontalAlignment = .left;
+        collegeButton.contentEdgeInsets = buttonTextEdgeInset;
+        collegeButton.contentHorizontalAlignment = .left;
+        athleticsButton.contentEdgeInsets = buttonTextEdgeInset;
+        athleticsButton.contentHorizontalAlignment = .left;
+        eventsButton.contentEdgeInsets = buttonTextEdgeInset;
+        eventsButton.contentHorizontalAlignment = .left;
+        referenceButton.contentEdgeInsets = buttonTextEdgeInset;
+        referenceButton.contentHorizontalAlignment = .left;
+        otherButton.contentEdgeInsets = buttonTextEdgeInset;
+        otherButton.contentHorizontalAlignment = .left;
+        
+        
+        
+        
     }
 
 }
