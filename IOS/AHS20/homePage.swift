b7e23a034b238112abab7e22e15281ad1bb2af24
richardwei6 <48197502+richardwei6@users.noreply.github.com>
2020-04-20 18:37:10 -0700

    Home page content finished

--- IOS/AHS20/homePage.swift
+++ IOS/AHS20/homePage.swift
@@ -62,6 +62,70 @@ class homeClass: UIViewController, UIScrollViewDelegate, UITabBarControllerDeleg
 	   }
 	
 	
+	func smallArticle(x: CGFloat, y: CGFloat, width: CGFloat, height: CGFloat, articleNum: Int) -> UIButton{//TODO: find out a way to separate article from top and bottom
+				
+		let articleFrame = CGRect(x: x, y: y, width: width, height: height);
+		let articleView = UIButton(frame: articleFrame);
+		
+		let articleTextWidth = (width/2) + 10;
+		
+		// article timestamp
+		let timestampFrame = CGRect(x: 0, y: 0, width: 70, height: 10);
+		let timestamp = UILabel(frame: timestampFrame);
+		timestamp.numberOfLines = 1;
+		timestamp.adjustsFontSizeToFitWidth = true;
+		timestamp.minimumScaleFactor = 0.5;
+		timestamp.textAlignment = .left;
+		timestamp.textColor = makeColor(r: 57, g: 57, b: 57);
+		timestamp.font = UIFont(name: "SFProDisplay-Regular", size: 10);
+		timestamp.text = "1 hour ago";
+		
+		
+		let articleTitleFrame = CGRect(x: 0, y: 10, width: articleTextWidth, height: height/2);
+		let articleTitleLabel = UILabel(frame: articleTitleFrame);
+		articleTitleLabel.numberOfLines = 2;
+		articleTitleLabel.adjustsFontSizeToFitWidth = true;
+		articleTitleLabel.minimumScaleFactor = 0.8;
+		articleTitleLabel.textAlignment = .left;
+		articleTitleLabel.font = UIFont(name: "SFProDisplay-Black",size: 20);
+		articleTitleLabel.text = "Auto Adjusting Long Title";
+		
+		
+		let articleSubtitleFrame = CGRect(x: 0, y: height/2, width: articleTextWidth, height: height/2);
+		let articleSubtitleContent = UILabel(frame: articleSubtitleFrame);
+		articleSubtitleContent.numberOfLines = 2;
+		articleSubtitleContent.textAlignment = .left;
+		articleSubtitleContent.font = UIFont(name: "SFProDisplay-Regular", size: 15);
+		articleSubtitleContent.text = "This is the content inside the article. What you are seeing is a preview of such article.";
+		
+		
+		let articleImageFrame = CGRect(x: articleTextWidth + 10, y: 10, width: width - (articleTextWidth + 20), height: height - 10);
+		let articleImageView = UIImageView(frame: articleImageFrame);
+		articleImageView.backgroundColor = makeColor(r: 143, g: 142, b: 142); // articleDarkGreyBackground
+		articleImageView.layer.cornerRadius = 10;
+		
+		// subview bookmark button - 30x30
+		let bookmarkFrame = CGRect(x: articleImageFrame.size.width - 40, y: 10, width: 30, height: 30);
+		let bookmarkButton = UIButton(frame: bookmarkFrame);
+		bookmarkButton.backgroundColor = makeColor(r: 216, g: 216, b: 216);
+		bookmarkButton.setRoundedEdge(corners: [.topRight,.topLeft,.bottomLeft,.bottomRight], radius: 6);
+		let bookmarkImage = UIImage(systemName: "bookmark"); // get system image
+		bookmarkButton.setImage(bookmarkImage, for: .normal);
+		
+		
+		//add bookmark button subview to imageview
+		articleImageView.addSubview(bookmarkButton);
+		
+		
+		//add all other elements to everything
+		articleView.addSubview(timestamp);
+		articleView.addSubview(articleTitleLabel);
+		articleView.addSubview(articleSubtitleContent);
+		articleView.addSubview(articleImageView);
+		
+		return articleView;
+	}
+	
 
 
     
@@ -82,7 +146,7 @@ class homeClass: UIViewController, UIScrollViewDelegate, UITabBarControllerDeleg
 		let scrollViewHorizontalConstraints = CGFloat(50);
 		
 		
-		// Featured News -----
+		// Featured News ----- NOTE - article is not created by smallArticle() func
 		featuredPageControl.numberOfPages = featuredSize;
 		featuredFrame.size = featuredScrollView.frame.size;
 		featuredFrame.size.width = UIScreen.main.bounds.size.width - scrollViewHorizontalConstraints;
@@ -154,29 +218,15 @@ class homeClass: UIViewController, UIScrollViewDelegate, UITabBarControllerDeleg
 			asbNewsFrame.origin.x = asbNewsFrame.size.width * CGFloat(aIndex);
 			
 			  
-			  // create content in scrollview
-			  let contentView = UIButton(frame: asbNewsFrame); // wrapper for article
-			  contentView.backgroundColor = makeColor(r: 147, g: 66, b: 78);
-						
-			  /*// content inside contentView -------
-			  let articleContentFrame = CGRect(x:0,y:0,width:districtNewsFrame.width,height:districtNewsFrame.height-60);
-			  let articleContent = UIView(frame: articleContentFrame); // may be image?
-			  articleContent.backgroundColor = makeColor(r: 138, g: 138, b: 138);
-						
-			  let articleLabelFrame = CGRect(x:0,y:districtNewsFrame.height-60,width:districtNewsFrame.width,height:60);
-			  let articleLabel = UILabel(frame: articleLabelFrame);
-			  articleLabel.text = "    " + "Title";
-			  articleLabel.backgroundColor = UIColor.white;
-			  articleLabel.font = UIFont(name: "DINCondensed-Bold", size: 30);
-						
-						
-						
-			  // add content inside contentView to contentview
-			  contentView.addSubview(articleContent);
-			  contentView.addSubview(articleLabel);
-			  // add contentview to scrollview*/
-			  contentView.setRoundedEdge(corners: [.topRight,.topLeft,.bottomLeft,.bottomRight], radius: 30);
-			  self.asbNewsScrollView.addSubview(contentView);
+			// create content in scrollview
+			let contentView = UIView(frame: asbNewsFrame); // wrapper for article
+			
+			//create article with function - TODO: find out a way to separate article from top and bottom
+			contentView.addSubview(smallArticle(x: 5, y: 0, width: asbNewsFrame.size.width-5, height: 120, articleNum: aIndex));
+			contentView.addSubview(smallArticle(x: 5, y: 120, width: asbNewsFrame.size.width-5, height: 120, articleNum: aIndex));
+			
+			
+			self.asbNewsScrollView.addSubview(contentView);
 		  }
 		  // change horizontal size of scrollview
 		  asbNewsScrollView.contentSize = CGSize(width: (asbNewsFrame.size.width * CGFloat(asbNewsSize)) , height: asbNewsScrollView.frame.size.height);
@@ -190,34 +240,16 @@ class homeClass: UIViewController, UIScrollViewDelegate, UITabBarControllerDeleg
 		sportsNewsFrame.size = sportsNewsScrollView.frame.size;
 		sportsNewsFrame.size.width = UIScreen.main.bounds.size.width - scrollViewHorizontalConstraints;
 		  for aIndex in 0..<sportsNewsSize{
-			  //districtNewsFrame.origin.x = (UIScreen.main.bounds.size.width-52) * CGFloat(aIndex);
-			  //districtNewsFrame.size = UIScreen.main.bounds.size;
 			sportsNewsFrame.origin.x = sportsNewsFrame.size.width * CGFloat(aIndex);
 				  
 			
 			   // create content in scrollview
-			   let contentView = UIButton(frame: sportsNewsFrame); // wrapper for article
-			   contentView.backgroundColor = makeColor(r: 147, g: 66, b: 78);
-						 
-			   // content inside contentView -------
-			  /* let articleContentFrame = CGRect(x:0,y:0,width:districtNewsFrame.width,height:districtNewsFrame.height-60);
-			   let articleContent = UIView(frame: articleContentFrame); // may be image?
-			   articleContent.backgroundColor = makeColor(r: 138, g: 138, b: 138);
-						 
-			   let articleLabelFrame = CGRect(x:0,y:districtNewsFrame.height-60,width:districtNewsFrame.width,height:60);
-			   let articleLabel = UILabel(frame: articleLabelFrame);
-			   articleLabel.text = "    " + "Title";
-	           articleLabel.backgroundColor = UIColor.white;
-			   articleLabel.font = UIFont(name: "DINCondensed-Bold", size: 30);
-						 
-						 
-						 
-			   // add content inside contentView to contentview
-			   contentView.addSubview(articleContent);
-			   contentView.addSubview(articleLabel);
-			   // add contentview to scrollview*/
-			   contentView.setRoundedEdge(corners: [.topRight,.topLeft,.bottomLeft,.bottomRight], radius: 30);
-			   self.sportsNewsScrollView.addSubview(contentView);
+			let contentView = UIView(frame: sportsNewsFrame); // wrapper for article
+			contentView.addSubview(smallArticle(x: 5, y: 0, width: asbNewsFrame.size.width-5, height: 120, articleNum: aIndex));
+			contentView.addSubview(smallArticle(x: 5, y: 120, width: asbNewsFrame.size.width-5, height: 120, articleNum: aIndex));
+			
+			
+			self.sportsNewsScrollView.addSubview(contentView);
 		  }
 		  // change horizontal size of scrollview
 		sportsNewsScrollView.contentSize = CGSize(width: (sportsNewsFrame.size.width * CGFloat(sportsNewsSize)), height: sportsNewsScrollView.frame.size.height);
@@ -229,44 +261,18 @@ class homeClass: UIViewController, UIScrollViewDelegate, UITabBarControllerDeleg
 		districtNewsFrame.size = districtNewsScrollView.frame.size;
 		districtNewsFrame.size.width = UIScreen.main.bounds.size.width - scrollViewHorizontalConstraints;
         for aIndex in 0..<districtNewsSize{
-            //districtNewsFrame.origin.x = (UIScreen.main.bounds.size.width-52) * CGFloat(aIndex);
-			//districtNewsFrame.size = UIScreen.main.bounds.size;
 			districtNewsFrame.origin.x = (districtNewsFrame.size.width * CGFloat(aIndex));
 			
             // create content in scrollview
-			let contentView = UIButton(frame: districtNewsFrame); // wrapper for article
-			//testButton.setImage(UIImage(named: "ahsldpi.png"), for: .normal);
-			contentView.backgroundColor = makeColor(r: 147, g: 66, b: 78);
+			let contentView = UIView(frame: districtNewsFrame); // wrapper for article
+			contentView.addSubview(smallArticle(x: 5, y: 0, width: asbNewsFrame.size.width-5, height: 120, articleNum: aIndex));
+			contentView.addSubview(smallArticle(x: 5, y: 120, width: asbNewsFrame.size.width-5, height: 120, articleNum: aIndex));
 			
-			/*// content inside contentView -------
-			let articleContentFrame = CGRect(x:0,y:0,width:districtNewsFrame.width,height:districtNewsFrame.height-60);
-			let articleContent = UIView(frame: articleContentFrame); // may be image?
-			articleContent.backgroundColor = makeColor(r: 138, g: 138, b: 138);
-			
-			let articleLabelFrame = CGRect(x:0,y:districtNewsFrame.height-60,width:districtNewsFrame.width,height:60);
-			let articleLabel = UILabel(frame: articleLabelFrame);
-			articleLabel.text = "    " + "Title";
-			articleLabel.backgroundColor = UIColor.white;
-			articleLabel.font = UIFont(name: "DINCondensed-Bold", size: 30);
-			
-			
-			
-			// add content inside contentView to contentview
-			contentView.addSubview(articleContent);
-			contentView.addSubview(articleLabel);
-            // add contentview to scrollview*/
-			contentView.setRoundedEdge(corners: [.topRight,.topLeft,.bottomLeft,.bottomRight], radius: 30);
             self.districtNewsScrollView.addSubview(contentView);
         }
         // change horizontal size of scrollview
 		districtNewsScrollView.contentSize = CGSize(width: (districtNewsFrame.size.width * CGFloat(districtNewsSize)), height: districtNewsScrollView.frame.size.height);
         districtNewsScrollView.delegate = self;
-		
-		
-		
-		
-		
-	  
         
     }
 	
