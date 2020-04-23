bd9192f47a29814efd4a5c3688b221bbc193bb82
richardwei6 <48197502+richardwei6@users.noreply.github.com>
2020-04-23 14:15:09 -0700

    Bulletin color changes

--- IOS/AHS20/bulletinPage.swift
+++ IOS/AHS20/bulletinPage.swift
@@ -41,6 +41,67 @@ class bulletinClass: UIViewController, UIScrollViewDelegate, UITabBarControllerD
     }
     
     
+    func generateBulletin(){
+        // set up bulletin
+        bulletinFrame.size.height = articleVerticalSize;
+        bulletinFrame.size.width = UIScreen.main.bounds.size.width - (2*articleHorizontalPadding);
+        
+        for aIndex in 0..<bulletinSize{
+            bulletinFrame.origin.x = articleHorizontalPadding;
+            bulletinFrame.origin.y = ((bulletinFrame.size.height+articleVerticalPadding)*CGFloat(aIndex));
+            let articleButton = UIView(frame: bulletinFrame);
+            articleButton.backgroundColor = makeColor(r: 247, g: 238, b: 239);
+            
+            // content inside button
+            let mainViewFrame = CGRect(x: 10, y: 10, width: bulletinFrame.size.width - (2*articleHorizontalPadding) - 20, height: bulletinFrame.size.height - 10);
+            let mainView = UIButton(frame : mainViewFrame);
+            
+            let articleIconFrame = CGRect(x: 0, y: 0, width: 40, height: 40);
+            let articleIcon = UIImageView(frame: articleIconFrame);
+            articleIcon.image = UIImage(named: "Group 33.png");
+            
+            let articleTitleFrame = CGRect(x: 50, y : 5, width: 100, height: 60);
+            let articleTitleText = UILabel(frame: articleTitleFrame);
+            articleTitleText.text = "Title"; // insert title text here
+            articleTitleText.font =  UIFont(name: "SFProDisplay-Regular",size: 30);
+            articleTitleText.numberOfLines = 1;
+            articleTitleText.adjustsFontSizeToFitWidth = true;
+            articleTitleText.sizeToFit();
+
+            
+            
+            let articleBodyFrame = CGRect(x: 0, y: 40, width: mainViewFrame.size.width, height: mainViewFrame.size.height - 50);
+            let articleBodyText = UILabel(frame: articleBodyFrame);
+            articleBodyText.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec blandit erat, a pellentesque urna. Donec eget tristique elit, non mattis mauris. Duis eget feugiat nisi, eget ornare velit. Maecenas a malesuada orci. Sed suscipit augue vitae turpis blandit, sit amet condimentum nulla blandit. Integer malesuada sed dolor vel ultrices. Aenean eget ligula pulvinar leo hendrerit ornare eget a augue. Aenean id hendrerit erat, in sodales massa. Etiam eu finibus justo. Morbi nunc eros, aliquam non erat eu, tincidunt vulputate mauris. Integer non nibh a nisi vestibulum condimentum. Etiam et sapien lacus. Donec sollicitudin, turpis quis aliquam hendrerit, sem arcu consectetur erat, sed bibendum sapien sapien vel ante. Duis eget mi feugiat, aliquet diam id, vehicula tellus. Maecenas et rutrum metus, in sodales nulla." // insert body text here
+            articleBodyText.numberOfLines = 4;
+            articleBodyText.font = UIFont(name: "SFProDisplay-Regular", size: 15);
+            
+            
+            mainView.addSubview(articleIcon);
+            mainView.addSubview(articleTitleText);
+            //mainView.addSubview(dateText);
+            mainView.addSubview(articleBodyText);
+            
+            let dateTextFrame = CGRect(x: bulletinFrame.size.width - (2*articleHorizontalPadding) - 95, y : 0, width: 100, height: 25);
+            let dateText = UILabel(frame: dateTextFrame);
+            dateText.text = "00/00/0000"; // insert date here
+            dateText.textColor = makeColor(r: 189, g: 151, b: 104);
+            dateText.textAlignment = .right;
+
+            
+            articleButton.addSubview(dateText);
+            articleButton.addSubview(mainView);
+            
+            articleButton.layer.cornerRadius = 10;
+            
+            mainView.addTarget(self, action: #selector(self.openArticle), for: .touchUpInside)
+            self.bulletinScrollView.addSubview(articleButton);
+        }
+        bulletinScrollView.contentSize = CGSize(width: bulletinFrame.size.width-(2*articleHorizontalPadding), height: articleHorizontalPadding+(bulletinFrame.size.height+articleHorizontalPadding)*CGFloat(bulletinSize));
+        bulletinScrollView.delegate = self;
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad();
        
@@ -83,64 +144,8 @@ class bulletinClass: UIViewController, UIScrollViewDelegate, UITabBarControllerD
         filterScrollView.delegate = self;
         
         
-        // set up bulletin
-        bulletinFrame.size.height = articleVerticalSize;
-        bulletinFrame.size.width = UIScreen.main.bounds.size.width - (2*articleHorizontalPadding);
-        
-        for aIndex in 0..<bulletinSize{
-            bulletinFrame.origin.x = articleHorizontalPadding;
-            bulletinFrame.origin.y = ((bulletinFrame.size.height+articleVerticalPadding)*CGFloat(aIndex));
-            let articleButton = UIView(frame: bulletinFrame);
-            articleButton.backgroundColor = makeColor(r: 204, g: 174, b: 148);
-            
-            // content inside button
-            let mainViewFrame = CGRect(x: 10, y: 10, width: bulletinFrame.size.width - (2*articleHorizontalPadding) - 20, height: bulletinFrame.size.height - 10);
-            let mainView = UIButton(frame : mainViewFrame);
-            
-            let articleIconFrame = CGRect(x: 0, y: 0, width: 40, height: 40);
-            let articleIcon = UIImageView(frame: articleIconFrame);
-            articleIcon.image = UIImage(named: "Group 33.png");
-            
-            let articleTitleFrame = CGRect(x: 50, y : 5, width: 100, height: 60);
-            let articleTitleText = UILabel(frame: articleTitleFrame);
-            articleTitleText.text = "Title"; // insert title text here
-            articleTitleText.font =  UIFont(name: "SFProDisplay-Regular",size: 30);
-            articleTitleText.numberOfLines = 1;
-            articleTitleText.adjustsFontSizeToFitWidth = true;
-            articleTitleText.sizeToFit();
-
-            
-            
-            let articleBodyFrame = CGRect(x: 0, y: 40, width: mainViewFrame.size.width, height: mainViewFrame.size.height - 50);
-            let articleBodyText = UILabel(frame: articleBodyFrame);
-            articleBodyText.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec blandit erat, a pellentesque urna. Donec eget tristique elit, non mattis mauris. Duis eget feugiat nisi, eget ornare velit. Maecenas a malesuada orci. Sed suscipit augue vitae turpis blandit, sit amet condimentum nulla blandit. Integer malesuada sed dolor vel ultrices. Aenean eget ligula pulvinar leo hendrerit ornare eget a augue. Aenean id hendrerit erat, in sodales massa. Etiam eu finibus justo. Morbi nunc eros, aliquam non erat eu, tincidunt vulputate mauris. Integer non nibh a nisi vestibulum condimentum. Etiam et sapien lacus. Donec sollicitudin, turpis quis aliquam hendrerit, sem arcu consectetur erat, sed bibendum sapien sapien vel ante. Duis eget mi feugiat, aliquet diam id, vehicula tellus. Maecenas et rutrum metus, in sodales nulla." // insert body text here
-            articleBodyText.numberOfLines = 4;
-            articleBodyText.font = UIFont(name: "SFProDisplay-Regular", size: 15);
-            
-            
-            mainView.addSubview(articleIcon);
-            mainView.addSubview(articleTitleText);
-            //mainView.addSubview(dateText);
-            mainView.addSubview(articleBodyText);
-            
-            let dateTextFrame = CGRect(x: bulletinFrame.size.width - (2*articleHorizontalPadding) - 100, y : 0, width: 100, height: 25);
-            let dateText = UILabel(frame: dateTextFrame);
-            dateText.text = "00/00/0000"; // insert date here
-            dateText.textColor = UIColor.white;
-            dateText.textAlignment = .right;
-
-            
-            articleButton.addSubview(dateText);
-            articleButton.addSubview(mainView);
-            
-            articleButton.layer.cornerRadius = 10;
-            
-            mainView.addTarget(self, action: #selector(self.openArticle), for: .touchUpInside)
-            self.bulletinScrollView.addSubview(articleButton);
-        }
-        bulletinScrollView.contentSize = CGSize(width: bulletinFrame.size.width-(2*articleHorizontalPadding), height: articleHorizontalPadding+(bulletinFrame.size.height+articleHorizontalPadding)*CGFloat(bulletinSize));
-        bulletinScrollView.delegate = self;
-        
+       // set up bulletin for the first time before any filters
+        generateBulletin();
         
         
     }
