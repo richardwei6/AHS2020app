3e29cec633b720074948450e5e42322f56622e24
richardwei6 <48197502+richardwei6@users.noreply.github.com>
2020-05-04 20:43:12 -0700

    New Tab Bar

--- IOS/AHS20/CustomUITabController.swift
+++ IOS/AHS20/CustomUITabController.swift
@@ -9,17 +9,74 @@
 import Foundation
 import UIKit
 
-class CustomTabBarController: UITabBarController {
+class CustomTabBarController: UIViewController {
 
-@IBOutlet weak var profileTabBar: UITabBar!
+    @IBOutlet weak var contentView: UIView!
+    
+    @IBOutlet weak var tabBarView: UIView!
+    
+    @IBOutlet var buttons: [UIButton]!
+    
+    var homeViewController: UIViewController!
+    var bulletinViewController: UIViewController!
+    var savedViewController: UIViewController!
+    var settingsViewController: UIViewController!
+    
+    var viewControllers: [UIViewController]!
+    
+    var selectedIndex: Int = 0;
 
- override func viewDidLoad() {
-     super.viewDidLoad()
-    // Set the size and the position in the screen of the tab bar
+    let iconImagePath = ["house.fill", "doc.plaintext", "bookmark", "gear"];
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        contentView.bottomAnchor.constraint(equalToSystemSpacingBelow: view.bottomAnchor, multiplier: 1).isActive = true;
+        
+        tabBarView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -25).isActive = true;
+        tabBarView.layer.cornerRadius = 30;
+       // tabBarView.frame.size.height = CGFloat(70);
+        
+
+        // set up buttons
+        for index in 0..<buttons.count{
+            var image = UIImage(systemName: iconImagePath[index]);
+            image = image?.maskWithColor(color: UIColor.white);
+            buttons[index].setImage(image, for: .normal);
+            buttons[index].tintColor = UIColor.white;
+        }
+        
+        
+        let storyboard = UIStoryboard(name: "Main", bundle: nil);
+        homeViewController = storyboard.instantiateViewController(withIdentifier: "homeViewController");
+        bulletinViewController = storyboard.instantiateViewController(withIdentifier: "bulletinViewController");
+        savedViewController = storyboard.instantiateViewController(withIdentifier: "savedViewController");
+        settingsViewController = storyboard.instantiateViewController(withIdentifier: "settingsViewController");
+        viewControllers = [homeViewController, bulletinViewController, savedViewController, settingsViewController];
+        buttons[selectedIndex].isSelected = true;
+        didPressTab(buttons[selectedIndex]);
     }
     
-    override func viewWillLayoutSubviews() {
-        //TabBar.frame = CGRect(x: 0, y: 0, width: TabBar.frame.size.width, height: TabBar.frame.size.height)
-        super.viewWillLayoutSubviews()
+    @IBAction func didPressTab(_ sender: UIButton) {
+        let prevIndex = selectedIndex;
+        selectedIndex = sender.tag;
+        
+        // remove prev view controller
+        buttons[prevIndex].isSelected = false;
+        let prevVC = viewControllers[prevIndex];
+        prevVC.willMove(toParent: nil);
+        prevVC.view.removeFromSuperview();
+        prevVC.removeFromParent();
+        
+        // add current view controller
+        sender.isSelected = true;
+        let vc = viewControllers[selectedIndex];
+        addChild(vc);
+        vc.view.frame = contentView.bounds;
+        contentView.addSubview(vc.view);
+        vc.didMove(toParent: self);
     }
+    
+    
 }
--- IOS/AHS20/sharedFunc.swift
+++ IOS/AHS20/sharedFunc.swift
@@ -95,6 +95,33 @@ extension UIImageView {
   }
 }
 
+extension UIImage {
+
+    func maskWithColor(color: UIColor) -> UIImage? {
+        let maskImage = cgImage!
+
+        let width = size.width
+        let height = size.height
+        let bounds = CGRect(x: 0, y: 0, width: width, height: height)
+
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue)
+        let context = CGContext(data: nil, width: Int(width), height: Int(height), bitsPerComponent: 8, bytesPerRow: 0, space: colorSpace, bitmapInfo: bitmapInfo.rawValue)!
+
+        context.clip(to: bounds, mask: maskImage)
+        context.setFillColor(color.cgColor)
+        context.fill(bounds)
+
+        if let cgImage = context.makeImage() {
+            let coloredImage = UIImage(cgImage: cgImage)
+            return coloredImage
+        } else {
+            return nil
+        }
+    }
+
+}
+
 class SegueFromRight: UIStoryboardSegue {
     override func perform() {
         let src = self.source
@@ -103,7 +130,7 @@ class SegueFromRight: UIStoryboardSegue {
         src.view.superview?.insertSubview(dst.view, aboveSubview: src.view)
         dst.view.transform = CGAffineTransform(translationX: src.view.frame.size.width, y: 0)
 
-        UIView.animate(withDuration: 0.25,
+        UIView.animate(withDuration: 0.20,
                               delay: 0.0,
                             options: .curveEaseInOut,
                          animations: {
