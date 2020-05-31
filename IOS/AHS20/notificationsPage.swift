56c04ff34f6d6d02ef42100ed3ae0c51e6299eb1
richardwei6 <48197502+richardwei6@users.noreply.github.com>
2020-05-30 21:47:19 -0700

    Fixed icons and notifications

--- IOS/AHS20/notificationsPage.swift
+++ IOS/AHS20/notificationsPage.swift
@@ -32,6 +32,8 @@ class notificationsClass: UIViewController, UIScrollViewDelegate, UITabBarContro
     
     var horizontalPadding = CGFloat(10);
     var verticalPadding = CGFloat(10);
+    
+    let timeStampLength = CGFloat(100);
 
     
     func loadScrollView(){
@@ -48,47 +50,49 @@ class notificationsClass: UIViewController, UIScrollViewDelegate, UITabBarContro
             notificationFrame.origin.y = verticalPadding+((notificationFrame.size.height + verticalPadding)*CGFloat(nIndex));
             
             let notificationButton = notificationUIButton(frame: notificationFrame);
-            notificationButton.backgroundColor = makeColor(r: 244, g: 244, b: 245);
             
             
             let readIndicatorWidth = CGFloat(20);
             let readIndicatorFrame = CGRect(x: 0, y: 0, width: readIndicatorWidth, height: notificationFrame.size.height);
             let readIndicator = UIView(frame: readIndicatorFrame);
-            if (nIndex+1<=unreadNotificationSize){
-                readIndicator.backgroundColor = makeColor(r: 230, g: 205, b: 85);
-                notificationButton.unreadBool = true;
-            }
-            else{
-                readIndicator.backgroundColor = makeColor(r: 204, g: 204, b: 205);
-            }
             
             let chevronWidth = CGFloat(22);
-            let chevronFrame = CGRect(x: notificationFrame.size.width-chevronWidth-15, y: (notificationFrame.size.height/2)-(chevronWidth/2), width: chevronWidth, height: chevronWidth);
+            let chevronFrame = CGRect(x: notificationFrame.size.width-chevronWidth-15, y: (notificationFrame.size.height/2)-(chevronWidth/2), width: chevronWidth-5, height: chevronWidth);
             let chevronImage = UIImageView(frame: chevronFrame);
             chevronImage.image = UIImage(systemName: "chevron.right");
             chevronImage.tintColor = UIColor.gray;
             
             
-            let notificationTitleFrame = CGRect(x: readIndicatorWidth + 5, y: 0, width: 200, height: 50);
+            let notificationTitleFrame = CGRect(x: readIndicatorWidth + 7, y: 0, width: notificationFrame.size.width - readIndicatorWidth - chevronWidth - timeStampLength, height: 50);
             let notificationTitle = UILabel(frame: notificationTitleFrame);
-            notificationTitle.text = "Title";
+            notificationTitle.text = "Long Title";
             notificationTitle.font = UIFont(name:"SFProText-Bold",size: 22);
             
             
-            let notificationBodyFrame = CGRect(x: readIndicatorWidth + 8, y: notificationTitleFrame.size.height-15, width: notificationFrame.size.width - readIndicatorWidth - chevronWidth - 25, height: notificationFrame.size.height - (notificationTitleFrame.size.height-11));
+            let notificationBodyFrame = CGRect(x: readIndicatorWidth + 10, y: notificationTitleFrame.size.height-15, width: notificationFrame.size.width - readIndicatorWidth - chevronWidth - 27, height: notificationFrame.size.height - (notificationTitleFrame.size.height-11));
             let notificationBodyText = UILabel(frame: notificationBodyFrame);
             notificationBodyText.text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec blandit erat, a pellentesque urna. Donec eget tristique elit, non mattis mauris.";
             notificationBodyText.numberOfLines = 3;
             notificationBodyText.font = UIFont(name:"SFProDisplay-Regular",size: 16);
             
             
-            let timeStampLength = CGFloat(100);
             let timeStampFrame = CGRect(x: notificationFrame.size.width - chevronWidth - timeStampLength, y: 10, width: timeStampLength, height: 30);
             let timeStamp = UILabel(frame: timeStampFrame);
             timeStamp.text = "00 hours ago";
             timeStamp.font = UIFont(name:"SFProDisplay-Regular",size: 15);
             timeStamp.textAlignment = .left;
             timeStamp.textColor = UIColor.darkGray;
+            
+            
+            if (nIndex+1<=unreadNotificationSize){
+                readIndicator.backgroundColor = makeColor(r: 230, g: 205, b: 85);
+                notificationButton.backgroundColor = UIColor.white;
+                notificationButton.unreadBool = true;
+            }
+            else{
+                readIndicator.backgroundColor = makeColor(r: 204, g: 204, b: 205);
+                notificationButton.backgroundColor = makeColor(r: 244, g: 244, b: 245);
+            }
             
             
             notificationButton.addSubview(chevronImage);
