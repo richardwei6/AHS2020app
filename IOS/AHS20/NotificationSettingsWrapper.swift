09535dca11b2908a4102d8031a5bf140e70e1c0b
richardwei6 <48197502+richardwei6@users.noreply.github.com>
2020-05-31 22:40:59 -0700

    Fixed notifications page

---
+++ IOS/AHS20/NotificationSettingsWrapper.swift
@@ -0,0 +1,26 @@
+//
+//  NotificationSettingsWrapper.swift
+//  AHS20
+//
+//  Created by Richard Wei on 5/31/20.
+//  Copyright © 2020 AHS. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import AudioToolbox
+
+class notificationSettingsWrapperClass: UIViewController{
+    
+    @IBOutlet weak var notificationsContainer: UIView!
+    
+    @IBAction func dismiss(_ sender: Any) {
+        dismiss(animated: true);
+        AudioServicesPlaySystemSound(1519);
+    }
+    override func viewDidLoad(){
+        super.viewDidLoad();
+        notificationsContainer.bottomAnchor.constraint(equalToSystemSpacingBelow: view.bottomAnchor, multiplier: 1).isActive = true;
+    }
+    
+}
