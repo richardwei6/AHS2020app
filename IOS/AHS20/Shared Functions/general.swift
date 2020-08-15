076d636b7f75b3c461fc3466416f08a0d70a23cd
richardwei6 <48197502+richardwei6@users.noreply.github.com>
2020-08-15 14:17:27 -0700

    Added Optimization

--- IOS/AHS20/Shared Functions/general.swift
+++ IOS/AHS20/Shared Functions/general.swift
@@ -189,40 +189,12 @@ final class InsetLabel: UILabel {
     }
 }
 
-extension String {
-
-    var length: Int {
-        return count
-    }
-
-    subscript (i: Int) -> String {
-        return self[i ..< i + 1]
-    }
-
-    func substring(fromIndex: Int) -> String {
-        return self[min(fromIndex, length) ..< length]
-    }
-
-    func substring(toIndex: Int) -> String {
-        return self[0 ..< max(0, toIndex)]
-    }
-
-    subscript (r: Range<Int>) -> String {
-        let range = Range(uncheckedBounds: (lower: max(0, min(length, r.lowerBound)),
-                                            upper: min(length, max(0, r.upperBound))))
-        let start = index(startIndex, offsetBy: range.lowerBound)
-        let end = index(start, offsetBy: range.upperBound - range.lowerBound)
-        return String(self[start ..< end])
-    }
-}
-
 var utilNeedID: String?;
 
 func findArticleFromIDAndSegue(id: String){ // performs segue as well
     if (id == ""){
         return;
     }
-  //  print("ID in findArticleFromIDAndSegue - \(id)")
     utilNeedID = id;
     if (homeArticleList[0].count == 0 && homeArticleList[1].count == 0 && homeArticleList[2].count == 0){ // load homepage
         setUpConnection();
