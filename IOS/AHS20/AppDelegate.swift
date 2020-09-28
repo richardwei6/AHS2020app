72020ae408262b8528822d9a1e568fa0aaca84da
richardwei6 <48197502+richardwei6@users.noreply.github.com>
2020-09-27 20:12:52 -0700

    Added working sign in

--- IOS/AHS20/AppDelegate.swift
+++ IOS/AHS20/AppDelegate.swift
@@ -110,13 +110,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
     
     // GOOGLE SSO signin
     func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
-        
+        if let error = error {
+            if (error as NSError).code == GIDSignInErrorCode.hasNoAuthInKeychain.rawValue {
+                print("The user has not signed in before or they have since signed out.")
+            } else {
+                print("\(error.localizedDescription)")
+            }
+            return
+        }
+        /* Perform any operations on signed in user here.
+         let userId = user.userID                  // For client-side use only!
+         let idToken = user.authentication.idToken // Safe to send to the server
+         let fullName = user.profile.name
+         let givenName = user.profile.givenName
+         let familyName = user.profile.familyName
+         let email = user.profile.email
+         // ...*/
+        print("signed in - \(user.profile.email)")
     }
     
     func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!,
               withError error: Error!) {
-      // Perform any operations when the user disconnects from app here.
-      
+        // Perform any operations when the user disconnects from app here.
+        print("signed out functions");
     }
     
 }
--- IOS/AHS20/profilePage.swift
+++ IOS/AHS20/profilePage.swift
@@ -8,13 +8,58 @@
 
 import Foundation
 import UIKit
+import GoogleSignIn
 
 class profilePageClass: UIViewController{
     
     @IBOutlet weak var mainScrollView: UIScrollView!
     
+    @objc func signOutFunction(){
+        print("signed out");
+        GIDSignIn.sharedInstance()?.signOut();
+    }
+    
+    func renderView(){
+        // set up scroll view here
+        
+        for view in mainScrollView.subviews{
+            view.removeFromSuperview();
+        }
+        
+        let padding = CGFloat(10);
+        var scrollViewHeight = CGFloat(0);
+        
+        let signInButtonHeight = CGFloat(80);
+        let signInButton = GIDSignInButton();
+        signInButton.center = CGPoint(x: UIScreen.main.bounds.width/2, y: scrollViewHeight + padding + signInButtonHeight);
+        signInButton.frame.size.height = signInButtonHeight;
+        signInButton.frame.size.width = CGFloat(100);
+        
+        scrollViewHeight += signInButton.frame.height + padding + signInButtonHeight;
+        
+        mainScrollView.addSubview(signInButton);
+        
+        let signOutButtonWidth = CGFloat(100);
+        let signOutButtonFrame = CGRect(x: (UIScreen.main.bounds.width/2) - (signOutButtonWidth/2), y: padding + scrollViewHeight, width: signOutButtonWidth, height: 80);
+        let signOutButton = UIButton(frame: signOutButtonFrame);
+        signOutButton.backgroundColor = UIColor.black;
+        signOutButton.layer.cornerRadius = 5;
+        
+        signOutButton.addTarget(self, action: #selector(self.signOutFunction), for: .touchUpInside);
+        
+        scrollViewHeight += signOutButtonFrame.height + padding;
+        
+        mainScrollView.addSubview(signOutButton);
+        
+        mainScrollView.contentSize = CGSize(width: UIScreen.main.bounds.width, height: scrollViewHeight);
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad();
         
+        GIDSignIn.sharedInstance()?.presentingViewController = self;
+        GIDSignIn.sharedInstance()?.restorePreviousSignIn();
+        
+        renderView();
     }
 }
